e5a31b16fa6ee1d41decae8fd9afed23ba6db080
Fedi Nabli <fedinabli@gmail.com>
2023-02-25 09:43:23 +0100

    Configured routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BalanceComponent } from './balance/balance.component'
+import { DepositCoinsComponent } from './deposit-coins/deposit-coins.component'
+import { DepositNotesComponent } from './deposit-notes/deposit-notes.component'
+import { DepositComponent } from './deposit/deposit.component'
+import { WithdrawComponent } from './withdraw/withdraw.component'
+
+const routes: Routes = [
+  {
+    path: 'balance',
+    component: BalanceComponent
+  },
+  {
+    path: 'deposit',
+    component: DepositComponent
+  },
+  {
+    path: 'withdraw',
+    component: WithdrawComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/deposit/deposit.component.ts
+++ src/app/deposit/deposit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-deposit',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepositComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  coins() {
+    this.router.navigate(['coins'], { relativeTo: this.route });
+  }
+
+  notes() {
+    this.router.navigate(['notes'], { relativeTo: this.route });
   }
 
 }
