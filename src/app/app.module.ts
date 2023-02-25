e2e9832135ca7202b6a7992fc1a8f08a75c6886a
Fedi Nabli <fedinabli@gmail.com>
2023-02-25 09:12:56 +0100

    Created angular components and setup template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { BalanceComponent } from './balance/balance.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { DepositCoinsComponent } from './deposit-coins/deposit-coins.component';
+import { DepositNotesComponent } from './deposit-notes/deposit-notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepositComponent,
+    BalanceComponent,
+    WithdrawComponent,
+    DepositCoinsComponent,
+    DepositNotesComponent
   ],
   imports: [
     BrowserModule,
